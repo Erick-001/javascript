864b91868ecef79b9cab5627283e8d159d83214b
Erick-001 <erick.sts.eo@outlook.com>
2020-02-18 21:38:43 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const Print = require('.modulos/Output')
+
+function crip()
+{
+    var crip = document.getElementById('txtCrip')
+    
+}
+function desc()
+{
+    alert('está funcionando')
+}
+function info()
+{
+    alert('está funcionando')
+}
